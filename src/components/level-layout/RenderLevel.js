e255ee08f34c98b319de6f8aca7501c4a354977c
Kean Bayneslow <kean.bayneslow@gmail.com>
2023-07-16 17:01:15 +1000

    adds background themes and rendering

--- src/components/level-layout/RenderLevel.js
+++ src/components/level-layout/RenderLevel.js
@@ -1,10 +1,16 @@
 import styles from "./RenderLevel.module.css";
 import Sprite from "../object-graphics/Sprite.js"
-import { CELL_SIZE } from "../../helpers/const";
+import { CELL_SIZE, LEVEL_THEMES, THEME_BACKGROUNDS } from "../../helpers/const";
+import LevelBackgroundTilesLayer from './LevelBackgroundTilesLayer.js';
 
 export default function RenderLevel({ spriteSheetImage }) {
 
     const level = {
+
+        theme: LEVEL_THEMES.YELLOW,
+        tilesWidth: 8,
+        tilesHeight: 8,
+
         placements: [
             { id: 0, x: 0, y: 0, frameCoord: "0x2" },
             { id: 1, x: 1, y: 1, frameCoord: "0x2" },
@@ -18,8 +24,12 @@ export default function RenderLevel({ spriteSheetImage }) {
     };
 
     return (
-        <div className={styles.fullScreenContainer}>
+        <div className={styles.fullScreenContainer} style={{
+            background: THEME_BACKGROUNDS[level.theme]
+        }}>
             <div className={styles.gameScreen}>
+
+                <LevelBackgroundTilesLayer level={level} image={spriteSheetImage}/>
 
                 {
                     level.placements.map((placement) => {
